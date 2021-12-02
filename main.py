afffcfa456b0954234e0231dfe6dce738b7379e9
Thefubuking <eden.lecarpentier.simplon@gmail.com>
2021-12-02 11:22:41 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title("Recognition digite")
+st.write('''
+This app will predict the number you will import has a image 
+
+''')
+
+
+if(st.button("this will display a number")):
+    st.image("1.png")
